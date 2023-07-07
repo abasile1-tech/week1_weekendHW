be3bf3c73ab1883c0bbc644635a067e4526b6791
John Basile <ashton.basile@gmail.com>
2023-07-07 13:08:57 +0100

    Completed exercise 9 of users_exercise

--- users_exercise.py
+++ users_exercise.py
@@ -83,4 +83,11 @@ print(users["Erik"]["lottery_numbers"])
 users["Erik"]["home_town"] = "Edinburgh"
 print(users["Erik"]["home_town"])
 # 9. Add a pet dog to Erik called "fluffy"
+users["Erik"]["pets"].append(
+	{
+		"name": "fluffy",
+		"species": "dog"
+		}
+)
+print(users["Erik"]["pets"])
 # 10. Add another person to the users dictionary
